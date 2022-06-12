cbafc3d51c4adbd94934ffef62f30f5a84b3660f
nurrizkiadip <nrap.rizki10@gmail.com>
2022-06-12 23:55:33 +0700

    FInish challenge

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emotion) => {
+  try {
+    const responseIXX = await promiseTheaterIXX();
+    const responseVGC = await promiseTheaterVGC();
+    const responses = responseIXX.concat(responseVGC);
+
+    let jmlEmosi = 0;
+    responses.map(item => item.hasil === emotion && (jmlEmosi += 1));
+    return jmlEmosi;
+  } catch (error) {
+    return `Error: ${error}`;
+  }
+};
 
 module.exports = {
   promiseOutput,
